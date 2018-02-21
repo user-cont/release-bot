15405956ff3e5a85231b9407d236de5e95e325a3
Jan Koscielniak <jkosciel@redhat.com>
2018-02-21 11:00:53 +0100

    [WIP] Releasing to Fedora

--- bot.py
+++ bot.py
@@ -9,14 +9,19 @@ import os
 import getopt
 import yaml
 import subprocess
+import shutil
 
 conf = {"repository_name": '',
         "repository_owner": '',
         "github_token": '',
         "refresh_interval": 3 * 60,
         "debug": False,
-        "configuration": ''}
+        "configuration": '',
+        "fedora_branches": []}
 required_items = ['repository_name', 'repository_owner', 'github_token']
+api_endpoint = "https://api.github.com/graphql"
+api3_endpoint = "https://api.github.com/"
+pypi_url = "https://pypi.python.org/pypi/"
 
 
 def print_help():
@@ -31,10 +36,10 @@ def print_help():
 def debug_print(level=0, message=""):
     levels = ["[DEBUG]", "[WARNING]", "[ERROR]"]
     if conf['debug'] or level > 0:
-        print(levels[level] + " " + message, file=sys.stderr if level > 0 else sys.stdout + "\n")
+        print(levels[level] + " " + message + "\n", file=sys.stderr if level > 0 else sys.stdout)
 
 
-def parse_arguments(conf):
+def parse_arguments():
     opts, args = getopt.getopt(sys.argv[1:], "hdc:", ["help", "debug", "configuration="])
     for opt, arg in opts:
         if opt == '-h' or opt == '--help':
@@ -51,7 +56,7 @@ def parse_arguments(conf):
             conf['configuration'] = path
 
 
-def load_configuration(conf):
+def load_configuration():
     if len(conf['configuration']) <= 0:
         # configuration not supplied, look for conf.yaml in cwd
         path = os.path.join(os.getcwd(), 'conf.yaml')
@@ -72,19 +77,10 @@ def load_configuration(conf):
             sys.exit(1)
 
 
-parse_arguments(conf)
-load_configuration(conf)
-
-api_endpoint = "https://api.github.com/graphql"
-api3_endpoint = "https://api.github.com/"
-headers = {'Authorization': 'token %s' % conf['github_token']}
-pypi_url = "https://pypi.python.org/pypi/" + conf['repository_name'] + "/json"
-debug = True
-
-
 def send_query(query):
     query = {"query": 'query {repository(owner: "' + conf['repository_owner'] + '", name: "' + conf[
         'repository_name'] + '") {' + query + '}}'}
+    headers = {'Authorization': 'token %s' % conf['github_token']}
     debug_print(message=str(query))
     return requests.post(url=api_endpoint, json=query, headers=headers)
 
@@ -111,7 +107,7 @@ def parse_changelog(previous_version, version, path):
 
 
 def get_latest_version_pypi():
-    r = requests.get(url=pypi_url)
+    r = requests.get(url=pypi_url + str(conf['repository_name']) + '/json')
     if r.status_code == 200:
         return r.json()['info']['version']
     else:
@@ -130,6 +126,116 @@ def release_on_pypi(project_root):
         if p.returncode != 0:
             err = err.decode('utf-8').strip()
             debug_print(2, "PyPi release failed for some reason. Here's why:\n" + err)
+            sys.exit(1)
+
+
+def release_in_fedora(project_root, new_version):
+    tmp = tempfile.TemporaryDirectory()
+
+    # clone the repository from dist-git and change into that directory and switch to master
+    p = subprocess.Popen(
+        "cd " + tmp.name + " && fedpkg clone " + conf['repository_name'] + " && cd " + conf[
+            "repository_name"] + " && fedpkg switch-branch master",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+    output, err = p.communicate()
+    debug_print(0, output.decode('utf-8').strip())
+    if p.returncode != 0:
+        err = err.decode('utf-8').strip()
+        debug_print(2, "Cloning fedora repository failed:\n" + err)
+        sys.exit(1)
+
+    # this is now source directory
+    fedpkg_src = tmp.name + "/" + str(conf['repository_name'])
+
+    # retrieve sources
+    p = subprocess.Popen(
+        "cd " + fedpkg_src + " && fedpkg sources",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+    output, err = p.communicate()
+    debug_print(0, output.decode('utf-8').strip())
+    if p.returncode != 0:
+        err = err.decode('utf-8').strip()
+        debug_print(2, "Cloning fedora repository failed:\n" + err)
+        sys.exit(1)
+
+    # copy new spec file
+    src = project_root + "/" + str(conf["repository_name"]) + ".spec"
+    dst = fedpkg_src + "/" + str(conf["repository_name"]) + ".spec"
+    shutil.copy(src, dst)
+
+    dir_listing = os.listdir(fedpkg_src)
+
+    # get new source
+    p = subprocess.Popen(
+        "cd " + fedpkg_src + " && spectool -g *spec",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+    output, err = p.communicate()
+    debug_print(0, output.decode('utf-8').strip())
+    if p.returncode != 0:
+        err = err.decode('utf-8').strip()
+        debug_print(2, "Fetching new source from PyPi failed:\n" + err)
+        sys.exit(1)
+
+    # add new sources
+    dir_new_listing = os.listdir(fedpkg_src)
+    sources = ""
+    for item in dir_new_listing:
+        if item not in dir_listing:
+            # this is a new file therefore it should be added to sources
+            sources += item + " "
+
+    # if there are no new sources, abort update
+    if len(sources.strip()) <= 0:
+        debug_print(1, "There are no new sources, won't continue releasing to fedora")
+        tmp.cleanup()
+        return
+
+    cmd = "fedpkg new-sources " + sources
+    p = subprocess.Popen(
+        "cd " + fedpkg_src + " && " + cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+    output, err = p.communicate()
+    debug_print(0, output.decode('utf-8').strip())
+    if p.returncode != 0:
+        err = err.decode('utf-8').strip()
+        debug_print(2, "Adding new sources failed:\n" + err)
+        sys.exit(1)
+
+    # commit this change and start a build
+    commit_msg = "Update to " + new_version
+    p = subprocess.Popen(
+        "cd " + fedpkg_src + ' && fedpkg commit -m "' + commit_msg + '" && fedpkg push && fedpkg build',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+    output, err = p.communicate()
+    debug_print(0, output.decode('utf-8').strip())
+    if p.returncode != 0:
+        err = err.decode('utf-8').strip()
+        debug_print(2, "Committing and building failed:\n" + err)
+        sys.exit(1)
+
+    # cycle through other branches and merge the changes there, push, build
+    for branch in conf['fedora_branches']:
+        p = subprocess.Popen(
+            "cd " + fedpkg_src + " && fedpkg switch-branch " + str(
+                branch) + " && git merge master && fedpkg push && fedpkg build",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True)
+        output, err = p.communicate()
+        debug_print(0, output.decode('utf-8').strip())
+        if p.returncode != 0:
+            err = err.decode('utf-8').strip()
+            debug_print(2, "Merging on branch " + branch + " failed:\n" + err)
             sys.exit(1)
 
 
@@ -183,80 +289,89 @@ def version_tuple(v):
     return tuple(map(int, (v.split("."))))
 
 
-# check for closed merge requests
-latest = get_latest_version_pypi()
-cursor = ''
-found = False
-# try to find the latest release closed merge request
-while not found:
-    r = walk_through_closed_prs(cursor, 'before')
-    if len(r['data']['repository']['pullRequests']['edges']) == 0:
-        break
-    for edge in reversed(r['data']['repository']['pullRequests']['edges']):
-        print(edge['node']['title'])
-        cursor = edge['cursor']
-        if latest + ' release' == edge['node']['title'].lower():
-            print('found!')
-            found = True
-# now walk through PRs since the latest version and check for a new one
-while True:
+def main():
+    parse_arguments()
+    load_configuration()
+    headers = {'Authorization': 'token %s' % conf['github_token']}
+
+    # check for closed merge requests
+    latest = get_latest_version_pypi()
+    cursor = ''
     found = False
-    new_release = {'version': '0.0.0', 'commitish': '', 'fs_path': '', 'tempdir': None}
+    # try to find the latest release closed merge request
     while not found:
-        r = walk_through_closed_prs(cursor)
-        if len(r['data']['repository']['pullRequests']['edges']) <= 0:
+        r = walk_through_closed_prs(cursor, 'before')
+        if len(r['data']['repository']['pullRequests']['edges']) == 0:
             break
-        for edge in r['data']['repository']['pullRequests']['edges']:
+        for edge in reversed(r['data']['repository']['pullRequests']['edges']):
             print(edge['node']['title'])
             cursor = edge['cursor']
-            if re.match('\d\.\d\.\d release', edge['node']['title'].lower()):
-                version = edge['node']['title'].split()
-                new_release['version'] = version[0]
-                new_release['commitish'] = edge['node']['mergeCommit']['oid']
+            if latest + ' release' == edge['node']['title'].lower():
+                print('found!')
                 found = True
+    # now walk through PRs since the latest version and check for a new one
+    while True:
+        found = False
+        new_release = {'version': '0.0.0', 'commitish': '', 'fs_path': '', 'tempdir': None}
+        while not found:
+            r = walk_through_closed_prs(cursor)
+            if len(r['data']['repository']['pullRequests']['edges']) <= 0:
+                break
+            for edge in r['data']['repository']['pullRequests']['edges']:
+                print(edge['node']['title'])
+                cursor = edge['cursor']
+                if re.match('\d\.\d\.\d release', edge['node']['title'].lower()):
+                    version = edge['node']['title'].split()
+                    new_release['version'] = version[0]
+                    new_release['commitish'] = edge['node']['mergeCommit']['oid']
+                    found = True
 
-    # if found, make a new release on github
-    # this has to be done using older github api because v4 doesn't support this yet
-    if found:
-        debug_print(message='found version: ' + new_release['version'] + ' , commit id: ' + new_release['commitish'])
-        payload = {"tag_name": new_release['version'],
-                   "target_commitish": new_release['commitish'],
-                   "name": new_release['version'],
-                   "prerelease": False,
-                   "draft": False}
-        url = api3_endpoint + 'repos/' + conf['repository_owner'] + '/' + conf['repository_name'] + '/releases'
-        response = requests.post(url=url, headers=headers, json=payload)
-        if response.status_code != 201:
-            debug_print(2, "Something went wrong with creating new release on github:\n" + response.text)
-            sys.exit(1)
-        else:
-            # download the new release to a temporary directory
-            d = tempfile.TemporaryDirectory()
-            new_release['tempdir'] = d
-            info = json.loads(response.text)
-            r = requests.get(url=info['zipball_url'])
-            path = d.name + '/' + new_release['version']
-
-            # extract it
-            open(path + '.zip', 'wb').write(r.content)
-            archive = zipfile.ZipFile(path + '.zip')
-            archive.extractall(path=path)
-            dirs = os.listdir(path)
-            new_release['fs_path'] = path + "/" + dirs[0]
-
-            # parse changelog and update the release with it
-            changelog = parse_changelog(latest, new_release['version'], new_release['fs_path'])
-            url = api3_endpoint + 'repos/' + conf['repository_owner'] + '/' + conf[
-                'repository_name'] + '/releases/' + str(info['id'])
-            response = requests.post(url=url, json={'body': changelog}, headers=headers)
-            if response.status_code != 200:
-                print(2, "Something went wrong during changelog update for a release:\n" + response.text)
+        # if found, make a new release on github
+        # this has to be done using older github api because v4 doesn't support this yet
+        if found:
+            debug_print(message='found version: ' + new_release['version'] + ', commit id: ' + new_release['commitish'])
+            payload = {"tag_name": new_release['version'],
+                       "target_commitish": new_release['commitish'],
+                       "name": new_release['version'],
+                       "prerelease": False,
+                       "draft": False}
+            url = api3_endpoint + 'repos/' + conf['repository_owner'] + '/' + conf['repository_name'] + '/releases'
+            response = requests.post(url=url, headers=headers, json=payload)
+            if response.status_code != 201:
+                debug_print(2, "Something went wrong with creating new release on github:\n" + response.text)
                 sys.exit(1)
+            else:
+                # download the new release to a temporary directory
+                d = tempfile.TemporaryDirectory()
+                new_release['tempdir'] = d
+                info = json.loads(response.text)
+                r = requests.get(url=info['zipball_url'])
+                path = d.name + '/' + new_release['version']
 
-    latest = get_latest_version_pypi()
-    # check if a new release was made
-    if version_tuple(latest) < version_tuple(new_release['version']):
-        debug_print(0, "Newer version on github, triggering PyPi release")
-        release_on_pypi(new_release['fs_path'])
-        new_release['tempdir'].cleanup()
-    time.sleep(conf['refresh_interval'])
+                # extract it
+                open(path + '.zip', 'wb').write(r.content)
+                archive = zipfile.ZipFile(path + '.zip')
+                archive.extractall(path=path)
+                dirs = os.listdir(path)
+                new_release['fs_path'] = path + "/" + dirs[0]
+
+                # parse changelog and update the release with it
+                changelog = parse_changelog(latest, new_release['version'], new_release['fs_path'])
+                url = api3_endpoint + 'repos/' + conf['repository_owner'] + '/' + conf[
+                    'repository_name'] + '/releases/' + str(info['id'])
+                response = requests.post(url=url, json={'body': changelog}, headers=headers)
+                if response.status_code != 200:
+                    print(2, "Something went wrong during changelog update for a release:\n" + response.text)
+                    sys.exit(1)
+
+        latest = get_latest_version_pypi()
+        # check if a new release was made
+        if version_tuple(latest) < version_tuple(new_release['version']):
+            debug_print(0, "Newer version on github, triggering PyPi release")
+            release_on_pypi(new_release['fs_path'])
+            new_release['tempdir'].cleanup()
+        time.sleep(conf['refresh_interval'])
+
+
+if __name__ == '__main__':
+    main()
