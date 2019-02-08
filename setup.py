a956761cfdd6e5218d4d968b2dfa5b23671429c8
Jiri Popelka <jpopelka@redhat.com>
2019-02-08 16:52:49 +0100

    [setup.cfg] version & packages

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from pathlib import Path
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-version = {}
-with open("./release_bot/version.py") as fp:
-    exec(fp.read(), version)
-
-
-setup(version=version["__version__"],
-      packages=find_packages(exclude=['tests*']),
-      install_requires=Path("./requirements.txt").read_text(), )
+setup(install_requires=Path("./requirements.txt").read_text(), )
