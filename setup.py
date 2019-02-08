3a3c12c7ee27da88a976f96b24b1a385e17b13c4
Jiri Popelka <jpopelka@redhat.com>
2019-02-08 12:32:42 +0100

    setup.cfg

--- setup.py
+++ setup.py
@@ -5,33 +5,15 @@ except ImportError:
 
 
 def get_requirements():
-    with open("./requirements.txt") as file:
-        return file.readlines()
+    with open("./requirements.txt") as fp:
+        return fp.readlines()
 
 
 version = {}
 with open("./release_bot/version.py") as fp:
     exec(fp.read(), version)
 
-setup(
-    name='release-bot',
-    version=version["__version__"],
-    packages=['release_bot'],
-    python_requires='>=3.6',
-    url='https://github.com/user-cont/release-bot',
-    license='GPLv3+',
-    author='Red Hat',
-    author_email='user-cont@redhat.com',
-    description='Automated releasing from GitHub repositories',
-    install_requires=get_requirements(),
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3'
-    ],
-    entry_points={'console_scripts': [
-        'release-bot=release_bot.releasebot:main',
-    ]}
-)
+
+setup(version=version["__version__"],
+      packages=['release_bot'],
+      install_requires=get_requirements(), )
