6816c67946473b61a3586daf9b7406c6cc513afc
Radoslav Pitonak <rado.pitonak@gmail.com>
2019-08-23 09:57:44 +0200

    run celery

--- files/run.sh
+++ files/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-printf "release-bot:x:$(id -u):0:Release bot:/home/release-bot:/bin/bash\n" >>/home/release-bot/passwd
+printf "release-bot:x:$(id -u):0:Release bot:/home/release-bot:/bin/bash\n" >> /home/release-bot/passwd
 
 export RELEASE_BOT_HOME=/home/release-bot
 
-exec release-bot -c /secrets/prod/conf.yaml
+exec release-bot -c /home/release-bot/.config/conf.yaml &
+exec celery -A release_bot.celery_task worker -l info
